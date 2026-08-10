2e734620672118889eb866ec5e25c51e7a65e2a1
Jesús Castro <jtcastro98@gmail.com>
2026-08-10 12:12:06 -0500

    Redirrección correspondiente al tipo de usuario

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,50 @@ use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ActivityLogController;
 use App\Http\Controllers\DashboardController;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 */
+
+Route::get('/', function () {
+    if (!Auth::check()) {
+        return redirect()->route('login');
+    }
+    /** @var User $user */ $user = Auth::user();
+    if ($user->hasRole('user')) {
+        return redirect()->route('portal');
+    }
+    if ($user->hasAnyRole(['admin', 'super admin'])) {
+        return redirect()->route('dashboard');
+    }
+    Auth::logout();
+    return redirect()->route('login');
+})->name('home');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'role:super admin|admin'])->name('dashboard');
+
+//Rutas protegidas por autenticación
+Route::middleware('auth')->group(function () {
+    // SUPER ADMIN + ADMIN -> Admin Panel 
+    Route::middleware(['role:super admin|admin'])->group(function () {
+        Route::resource('users', UserController::class);
+        Route::resource('roles', RoleController::class);
+        Route::resource('permissions', PermissionController::class);
+        Route::get('activity-logs', [ActivityLogController::class, 'index'])->name('activity.logs.index');
+    });
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/portal', function () {
+        return view('portal.index');
+    })->name('portal');
+});
+require __DIR__ . '/auth.php';
+
 // Route::get('/', function () {
 //     return view('auth.login');
 // })->middleware('guest')->name('home');
@@ -22,45 +60,8 @@ use App\Http\Controllers\DashboardController;
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', function () {
-    return redirect()->route('login');
-})
-    ->middleware('guest')
-    ->name('home');
-
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware([
-        'auth',
-        'role:super admin|admin'
-    ])
-    ->name('dashboard');
-
-//Rutas protegidas por autenticación
-// Route::middleware('auth')->group(function () {
-Route::middleware('auth')->group(function () {
-
-    // SUPER ADMIN + ADMIN -> Admin Panel
-    Route::middleware(['role:super admin|admin'])->group(function () {
-        Route::resource('users', UserController::class);
-        Route::resource('roles', RoleController::class);
-        Route::resource('permissions', PermissionController::class);
-
-        Route::get('activity-logs', [ActivityLogController::class, 'index'])->name('activity.logs.index');
-    });
-
-    Route::get('/profile', [ProfileController::class, 'edit'])
-        ->name('profile.edit');
-
-    Route::patch('/profile', [ProfileController::class, 'update'])
-        ->name('profile.update');
-
-    Route::delete('/profile', [ProfileController::class, 'destroy'])
-        ->name('profile.destroy');
-
-    Route::get('/portal', function () {
-        return view('portal.index');
-    })
-        ->name('portal');
-});
-
-require __DIR__ . '/auth.php';
+// Route::get('/', function () {
+//     return redirect()->route('login');
+// })
+//     ->middleware('guest')
+//     ->name('home');
